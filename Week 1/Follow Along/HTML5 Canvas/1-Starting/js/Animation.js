9b9cc128e56d43887b85fa9c7317392431004025
HeadShotBoom <DCarroll@fullsail.edu>
2014-05-08 01:13:58 -0500

    Animation Complete

--- Week 1/Follow Along/HTML5 Canvas/1-Starting/js/Animation.js
+++ Week 1/Follow Along/HTML5 Canvas/1-Starting/js/Animation.js
@@ -4,9 +4,24 @@ window.onload = function() {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
 				
-			
+		var srcImg = document.getElementById('img1');
+
+        var rotation = 0;
+
+        ctx.translate(theCanvas.width/2,theCanvas.height/2);
+
+        setInterval(function(){
+
+            rotation+=8;
+
+            ctx.clearRect(-300,-300,theCanvas.width,theCanvas.height);
+
+            ctx.rotate(rotation*(Math.PI/180));
+            ctx.drawImage(srcImg,-75,-75);
+        },2000);
+
 						
 						
 		}
 	}
-}
+};

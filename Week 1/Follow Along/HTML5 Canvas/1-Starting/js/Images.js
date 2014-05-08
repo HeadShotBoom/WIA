04018f002cfbe8ef9312755c503c37c057076518
HeadShotBoom <DCarroll@fullsail.edu>
2014-05-07 23:42:57 -0500

    Done With images

--- Week 1/Follow Along/HTML5 Canvas/1-Starting/js/Images.js
+++ Week 1/Follow Along/HTML5 Canvas/1-Starting/js/Images.js
@@ -4,8 +4,31 @@ window.onload = function() {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
 
-				
-     						
+        //Create Variable to store image file
+        var srcImg = document.getElementById('img1');
+
+        //Draw image directly onto canvas
+//        ctx.drawImage(srcImg, 0,0);
+
+        //Draw a scaled down image
+        //drawImage(srcIma, dx, dy, dw, dh)
+//        ctx.drawImage(srcImg, 50,50,240, 300);
+
+        //Draw a slice of the image
+        //drawImage(srcImg, sourceyx, source-y, source-width, source-height, destination-x, destination-y, destination-width, destination-height)
+//        ctx.drawImage(srcImg, 285,40,95,140,50,50,190,280);
+
+        var srcVid = document.getElementById('vid1');
+        srcVid.play();
+
+        //setInterval(function, how often in ms);
+
+        setInterval(function(){
+
+            ctx.drawImage(srcVid,0,0,480,270);
+
+        },30);
+
 		}
 	}
-}
+};

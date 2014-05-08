e41b7d75f222ac71d396c3b35a3df5c950b4c415
HeadShotBoom <DCarroll@fullsail.edu>
2014-05-08 00:02:17 -0500

    Done With Clipping Paths

--- Week 1/Follow Along/HTML5 Canvas/1-Starting/js/Clipping.js
+++ Week 1/Follow Along/HTML5 Canvas/1-Starting/js/Clipping.js
@@ -3,9 +3,32 @@ window.onload = function() {
 	if (theCanvas && theCanvas.getContext) {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
-  				
-	
-     						
+
+        //Draw the image
+        var srcImg = document.getElementById('img1');
+
+//        ctx.drawImage(srcImg, 0,0);
+        /*
+        //Create a circle clipping path
+        ctx.beginPath();
+        ctx.arc(ctx.canvas.width/2, ctx.canvas.height/2, 150, 0, 2*Math.PI);
+        ctx.clip();
+        ctx.drawImage(srcImg, 0,0);
+        */
+
+        //Create a Path shape clipping mask
+        ctx.beginPath();
+        ctx.moveTo(105,200);
+        ctx.lineTo(250,40);
+        ctx.lineTo(525,50);
+        ctx.lineTo(475,385);
+        ctx.closePath();
+
+        ctx.clip();
+
+        ctx.drawImage(srcImg, 0,0);
+
+
 		}
 	}
-}
+};

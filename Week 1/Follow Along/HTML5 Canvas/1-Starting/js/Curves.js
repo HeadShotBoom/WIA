6990f99b50f7275ef5716b5f6675a3623bac5f5f
HeadShotBoom <DCarroll@fullsail.edu>
2014-05-07 21:33:52 -0500

    Done with Text

--- Week 1/Follow Along/HTML5 Canvas/1-Starting/js/Curves.js
+++ Week 1/Follow Along/HTML5 Canvas/1-Starting/js/Curves.js
@@ -3,7 +3,29 @@ window.onload = function() {
 	if (theCanvas && theCanvas.getContext) {
 	var ctx = theCanvas.getContext("2d");
 	if (ctx) {
-				
+
+        ctx.strokeStyle = 'blue';
+        ctx.lineWidth = 5;
+
+        //Stroke a simple bezier curve
+
+        ctx.beginPath();
+        ctx.moveTo(50,200);
+
+        //bezierCurveTo(control1 X, control1 y, control2 x, control2 y, x, y)
+
+        ctx.bezierCurveTo(50,100,200,300,200,150);
+
+        ctx.stroke();
+
+        //Quadratic Curve
+
+        ctx.beginPath();
+        ctx.moveTo(400,200);
+
+        //quadraticCurveTo(control x, control y, x, y)
+        ctx.quadraticCurveTo(400,100,600,150);
+        ctx.stroke();
 						
 						
 		}

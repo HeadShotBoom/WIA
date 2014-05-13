9fd987535570c991120305f5d6c6c00bf2714ba5
HeadShotBoom <DCarroll@fullsail.edu>
2014-05-12 23:55:07 -0500

    SoundJS done

--- Week 2/Follow Along/Starting AV Files/3-SoundJS/js/manifest.js
+++ Week 2/Follow Along/Starting AV Files/3-SoundJS/js/manifest.js
@@ -1,4 +1,39 @@
 window.onload = function() {
-			
+
+    if(!createjs.Sound.initializeDefaultPlugins()){return;}
+
+    //Create variable to hold path to audio files.
+
+    var audioPath = 'audio/'
+
+    //create an array of objects, each object is a sound
+    //Soundjs calls that a manifest
+
+    var manifest = [
+        {id:'Music', src:audioPath+'music.mp3|'+audioPath+'music.ogg'},
+        {id:'Thunder', src:audioPath+'thunder1.mp3|'+audioPath+'thunder1.ogg'}
+    ];
+
+    //Register manifest with soundjs
+
+    createjs.Sound.registerManifest(manifest);
+
+    //Listen for the files to load
+    createjs.Sound.addEventListener('fileload', handleLoad);
+
+    function handleLoad(event){
+        //This will run once each time a file is initally loaded
+        //Playing the file
+//        createjs.Sound.play(event.src);
+    }
 	
-	}	
+};
+
+function playMusic(){
+    //Play just music
+    createjs.Sound.play('Music');
+}
+
+function playThunder(){
+    createjs.Sound.play('Thunder');
+}
--- Week 2/Follow Along/Starting AV Files/3-SoundJS/js/simpleAudio.js
+++ Week 2/Follow Along/Starting AV Files/3-SoundJS/js/simpleAudio.js
@@ -1,5 +1,25 @@
 window.onload = function() {
-	
-	
-	
-	}
+
+    if(!createjs.Sound.initializeDefaultPlugins()){return;}
+	//SoundJS has the ability to preload sounds
+
+    createjs.Sound.registerSound({id:'soundId', src:'audio/music.mp3|audio/music.ogg'});
+
+    //Listen for the file to load
+    createjs.Sound.addEventListener('fileload', handleFileLoad);
+
+    function handleFileLoad(event){
+        //Sound has been loaded
+        //ONCE per every sound loaded
+
+        console.log('Preloaded:', event.src);
+
+        //Play Loaded Sound
+//        createjs.Sound.play(event.src);
+
+    }
+	};
+function playMusic(){
+    //Play the sound when button is clicked
+    createjs.Sound.play('soundId');
+}

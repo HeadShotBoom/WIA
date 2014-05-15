a2e2806de62ea1c56cb50c79c950e3eda0a9c912
HeadShotBoom <DCarroll@fullsail.edu>
2014-05-14 20:41:02 -0500

    ShowHide complete

--- Week 2/Follow Along/jQuery Animation/1-Starting/js/ShowHide.js
+++ Week 2/Follow Along/jQuery Animation/1-Starting/js/ShowHide.js
@@ -1,6 +1,16 @@
-$(function() { 
-	
-
+$(function() {
+    //Add a click function to our button
+    $('#hide').click(function(){
+        $('#theDiv').hide(2000, function(){
+            $('#theDiv').css("background-color", "#000");
+        });
+    });
+    $('#show').click(function(){
+        $('#theDiv').show(2000);
+    });
+    $('#toggle').click(function(){
+        $('#theDiv').toggle(1000);
+    })
 });
 
 

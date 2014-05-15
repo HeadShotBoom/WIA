0b4b9d6f52b7ce093d478963bc59d565d9bbdd75
HeadShotBoom <DCarroll@fullsail.edu>
2014-05-14 21:31:47 -0500

    Easing Complete

--- Week 2/Follow Along/jQuery Animation/1-Starting/js/Easing.js
+++ Week 2/Follow Along/jQuery Animation/1-Starting/js/Easing.js
@@ -1,24 +1,37 @@
-$(function() { 
+$(function() {
 
-            $("#right").click(function() {
-                $("#theDiv").animate({ width: "500px" }, 1000, "swing");
-            });
+    $(function() {
 
-            $("#text").click(function() {
-                $("#theDiv").animate({ fontSize: "24pt" }, 1000, "swing");
-            });
+        $('#right').click(function(){
+            $('#theDiv').animate({width:'500px'}, 1000, 'easeInCubic');
+        });
 
-            $("#move").click(function() {
-                $("#theDiv").animate({ left: "500" }, 1000, "swing");
-            });
-            
-            $("#multiple").click(function() {
-                $("#theDiv").animate({height: "500px", width: "500px", left: "500", fontSize: "24pt" }, 1000, "linear");
-            });
+        $('#text').click(function(){
+            $('#theDiv').animate({fontSize:"24pt"}, 1000, 'easeInOutElastic');
+        });
 
-            $("#reset").click(function() {
-                $("#theDiv").animate({ height: "180px", width: "250px", left: "0", fontSize: "16pt" }, 1000, "linear");
-             }); 
-					
- 
+        $('#move').click(function(){
+            $('#theDiv').animate({left:"500"}, 1000, 'swing');
+        });
+
+        $('#multiple').click(function(){
+            $('#theDiv').animate({height: "500px", width:'500px', left:'500', fontSize: '24pt'}, 1000, 'easeOutBounce');
+
+        });
+
+        $('#multiple2').click(function(){
+            //Chain each animate to the end of the last one excluding the JQuery selector and ending; until the very end.
+            $('#theDiv').animate({height:"500px"}, 1000, 'easeInBack').animate({width:"500px"}, 1000, 'easeOutBounce').animate({left:"500"}, 1000, 'easeInCubic').animate({fontSize:"24pt"}, 1000, 'swing');
+        });
+
+        $('#reset').click(function(){
+            //Pretty Much look in HTML and CSS to find initial values and animate to them
+            $('#theDiv').animate({height: "180px", width:'250px', left:'0', fontSize: '16pt'}, 1000, 'easeInOutBack');
+
+        });
+
+    });
+
+
+
 });

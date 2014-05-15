b97dc59513359df19ebc0db7e1ee4e4f9f035100
HeadShotBoom <DCarroll@fullsail.edu>
2014-05-14 20:49:28 -0500

    Fade complete

--- Week 2/Follow Along/jQuery Animation/1-Starting/js/Fade.js
+++ Week 2/Follow Along/jQuery Animation/1-Starting/js/Fade.js
@@ -1,5 +1,20 @@
-$(function() { 
-	
-	
-            
+$(function() {
+
+    $('#fadein').click(function(){
+        $('#theDiv').fadeIn(2000);
+    });
+
+    $('#fadeout').click(function(){
+        $('#theDiv').fadeOut(2000);
+    });
+
+    $('#fadeto3').click(function(){
+        $('#theDiv').fadeTo(2000,.3);
+    });
+
+    $('#fadeup').click(function(){
+        $('#theDiv').fadeTo(2000, 1);
+    })
+
+
 });

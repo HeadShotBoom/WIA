feb69fbd155267b8d65978e89dd53595f9eb96d0
HeadShotBoom <DCarroll@fullsail.edu>
2014-05-14 20:52:53 -0500

    Slide complete

--- Week 2/Follow Along/jQuery Animation/1-Starting/js/Sliding.js
+++ Week 2/Follow Along/jQuery Animation/1-Starting/js/Sliding.js
@@ -1,5 +1,16 @@
-$(function() { 
+$(function() {
 
+    $('#slideup').click(function(){
+       $('#theDiv').slideUp(2000);
+    });
+
+    $('#slidedown').click(function(){
+        $('#theDiv').slideDown(2000);
+    });
+
+    $('#toggle').click(function(){
+        $('#theDiv').slideToggle(2000);
+    });
 
 
 });

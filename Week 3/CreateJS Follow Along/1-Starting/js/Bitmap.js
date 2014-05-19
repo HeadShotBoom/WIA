546d27b411b5c972f183e04f48ad406e74f266ae
HeadShotBoom <DCarroll@fullsail.edu>
2014-05-19 15:02:40 -0500

    More Work Done

--- Week 3/CreateJS Follow Along/1-Starting/js/Bitmap.js
+++ Week 3/CreateJS Follow Along/1-Starting/js/Bitmap.js
@@ -4,7 +4,55 @@ window.onload = function() {
 			var canvas=document.getElementById("canvas");
 			
 			var stage= new createjs.Stage(canvas);
-			
-	
+
+            var bmp = new createjs.Bitmap();
+
+    //Load image in and wait till fully loaded to do anything with it.
+
+    var img = new Image();
+    img.src = "images/ship.svg";
+
+    img.onload = updateStage;
+
+    function updateStage(e){
+        bmp = new createjs.Bitmap(e.target);
+
+        //find the center of canvas
+        var centerX = canvas.width/2;
+        var centerY = canvas.height/2;
+
+        //Move ship to center of stage
+        bmp.x = centerX;
+        bmp.y = centerY;
+
+        //Change regristration point (Place in image that is center of image for alignment)
+        bmp.regX = 50;
+        bmp.regY = 50;
+
+        //Scale it up
+        bmp.scaleX = 4;
+        bmp.scaleY = 4;
+
+
+
+
+
+
+        stage.addChild(bmp);
+//        stage.update();
+    }
+
+
+    //Ticker controls timing
+    createjs.Ticker.addEventListener('tick', tick);
+
+    function tick(e){
+
+        bmp.rotation +=2;
+        bmp.x -= 2;
+        stage.update();
+
+    }
+
 		
-}
+};
--- Week 3/CreateJS Follow Along/1-Starting/js/Ticker.js
+++ Week 3/CreateJS Follow Along/1-Starting/js/Ticker.js
@@ -22,8 +22,39 @@ window.onload = function() {
 	
 	stage.addChild(shape);
 	
-	stage.update();
-	
-	
+//	stage.update();
+
+
+    //Setup Ticker Class, this controls time. Like a heartbeat that makes a "tick"
+    //Create a listener for that tick
+
+    //set framerate in FPS, default is 20
+    createjs.Ticker.setFPS(30);
+
+    //Create a listener
+    createjs.Ticker.addEventListener('tick', ticks);
+
+    function ticks(e){
+//        console.log('Tick');
+
+        shape.rotation+=4;
+
+//        shape.alpha -=.005;
+
+//        shape.x +=5;
+//        shape.y +=2;
+
+//        shape.skewX +=.1;
+//        shape.skewY +=20
+
+        shape.scaleX +=.1;
+        shape.scaleY +=10;
+
+
+        stage.update();
+
+    }
+
+
 			
-}
+};

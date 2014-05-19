6fe4fc0496be96b39b58207c2d4a9e21487e24fe
HeadShotBoom <DCarroll@fullsail.edu>
2014-05-19 14:09:55 -0500

    Text Done

--- Week 3/CreateJS Follow Along/1-Starting/js/Text.js
+++ Week 3/CreateJS Follow Along/1-Starting/js/Text.js
@@ -1,5 +1,41 @@
 window.onload = function() {
-	
-	
-	
-}
+
+    var canvas = document.getElementById('canvas1');
+    var stage = new createjs.Stage(canvas);
+
+
+    //Text class
+    var sentence = new createjs.Text();
+
+    //Modify the text property
+    sentence.text = 'Any Kind Of Text String.... Poop!';
+
+    //Font Property - Normal CSS
+    sentence.font = 'bold 30px Times';
+
+    //Color
+    sentence.color ='blue';
+
+    //Text is a display object
+    //It inherets all of the normal properties
+    sentence.x = 150;
+    sentence.y = 50;
+//    sentence.rotation = 45;
+
+
+    sentence.textAlign = 'left';
+
+    sentence.lineWidth = 170;
+
+    sentence.lineHeight = 40;
+
+    sentence.outline = false;
+
+    // Combine the above code into one line
+    var sentence2 = new createjs.Text('This is our Second Text!', 'bold 30px Times', 'blue');
+
+
+    stage.addChild(sentence2);
+    stage.addChild(sentence);
+    stage.update();
+};

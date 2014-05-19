88acd4dc8c9b53806f596716bd07efd61b275e15
HeadShotBoom <DCarroll@fullsail.edu>
2014-05-19 12:50:01 -0500

    Polystar done

--- Week 3/CreateJS Follow Along/1-Starting/js/Polystar.js
+++ Week 3/CreateJS Follow Along/1-Starting/js/Polystar.js
@@ -1,5 +1,36 @@
 window.onload = function() {
 	
+    var canvas = document.getElementById('canvas1');
+    var stage = new createjs.Stage(canvas);
+
+    var shape = new createjs.Shape();
+
+    shape.graphics.beginStroke('purple');
+    shape.graphics.setStrokeStyle(5, 'round', 'round');
+
+    //drawPolyStar(x, y, radius, #sides, point size, angle)
+    //pointSize -0- Polygon 1-Spikes
+    //5 side star
+    shape.graphics.drawPolyStar(75,75,50,5,.5, -90);
+
+    //a 3 sided star - triangle
+    shape.graphics.drawPolyStar(550,75,50,3,0, -90);
+
+    var shape2 = new createjs.Shape();
+    shape2.graphics.beginFill('red');
+    shape2.graphics.drawPolyStar(250,255,50,3,0, -90);
+
+
+    //Add event listener to make it clickable
+    shape2.addEventListener('click', function(event){
+        console.log('Red Triangle has been clicked!');
+    });
+
+
+
+
+    stage.addChild(shape);
+    stage.addChild(shape2);
+    stage.update();
 	
-	
-}
+};

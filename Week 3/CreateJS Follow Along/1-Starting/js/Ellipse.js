fc80179fd18a70d0324339e06e44e2bfd733b58d
HeadShotBoom <DCarroll@fullsail.edu>
2014-05-19 12:37:06 -0500

    Circles, YAY

--- Week 3/CreateJS Follow Along/1-Starting/js/Ellipse.js
+++ Week 3/CreateJS Follow Along/1-Starting/js/Ellipse.js
@@ -1,5 +1,26 @@
 window.onload = function() {
 	
-	
+	var canvas = document.getElementById('canvas1');
+    var stage = new createjs.Stage(canvas);
 
-}
+    var shape = new createjs.Shape();
+
+    //Create our stroke
+    shape.graphics.beginStroke('purple');
+
+    shape.graphics.setStrokeStyle(10, 'butt', 'round');
+
+    //Draw a circle
+    //drawCircle(x, y, radius)
+    shape.graphics.drawCircle(100,100,50);
+
+    //Draw an elipse    drawElipse(x, y, width, height)
+    shape.graphics.drawEllipse(75, 200, 150, 50);
+
+    //Draw Arcs   arc(x, y, radius, startAngle, endAngle, anticlockwise = 1 or clockwise = 0)
+    shape.graphics.beginStroke('red');
+    shape.graphics.arc(100,100,75, 0,-90*(Math.PI/180), 0);
+
+    stage.addChild(shape);
+    stage.update();
+};

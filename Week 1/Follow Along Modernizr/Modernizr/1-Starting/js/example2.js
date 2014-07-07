1cefeb2e6547cdeb063cf0a844c0fce708833c37
HeadShotBoom <DCarroll@fullsail.edu>
2014-07-07 13:56:11 -0500

    Sync

--- Week 1/Follow Along Modernizr/Modernizr/1-Starting/js/example2.js
+++ Week 1/Follow Along Modernizr/Modernizr/1-Starting/js/example2.js
@@ -6,7 +6,7 @@
 Modernizr.load({
     test:Modernizr.inputtypes.date,
     yep:'js/success.js',
-    nope:['js/fail.js', 'js'],
+    nope:['js/fail.js'],
     complete: function(){
         //Will run after js file is loaded base on case above
         console.log('Test is complete');
